d9088008cf9d07493474d4b5a4fd229edbdd45cb
oozzy <nozyio.hello@gmail.com>
2025-02-15 05:50:46 +0000

    update bench script for diff fuse

---
+++ bench-goofys.sh
@@ -0,0 +1,31 @@
+# Define a variable for the bedrock directory
+MOUNT_POINT="$HOME/bedrock"
+S3_URL="ffbox-ea1"
+LOG_FILE="$HOME/ffbox_mount.log"
+
+fusermount -uz $MOUNT_POINT
+mkdir -p $MOUNT_POINT
+
+
+goofys --file-mode=0755 $S3_URL $MOUNT_POINT 
+sleep 3  # Wait for 3 seconds to ensure the mount is available
+
+start_time=$(date +%s)
+echo "Start time: $(date)"
+
+cd $MOUNT_POINT/pyinstall_sdxl_gen/
+
+# ./main/main
+strace -tt -T -e trace=read,file -o "$HOME/strace_goofys.log" main/main
+# strace -tt -T -f -e trace=all -e signal=all -o "$HOME/pyinstall_ffbox_bench11.log" main/main
+
+end_time=$(date +%s)
+echo "End time: $(date)"
+
+time_diff=$((end_time - start_time))
+minutes=$((time_diff / 60))
+seconds=$((time_diff % 60))
+echo "Execution time: $minutes min $seconds seconds"
+
+# Unmount the bedrock directory
+fusermount -uz $MOUNT_POINT
--- bench-rclone.sh
+++ bench-rclone.sh
@@ -1,10 +1,10 @@
-MOUNT_POINT=~/rclone_bedrock
+MOUNT_POINT=~/bedrock
 fusermount -uz $MOUNT_POINT
 rm -rf "$HOME/.cache/rclone"
 mkdir -p "$HOME/.cache/rclone"
 mkdir -p $MOUNT_POINT
 
-yes |  rclone mount  "s3://ffbox-ea1/pyinstall_sdxl_gen/" $MOUNT_POINT \
+yes |  rclone mount "s3://ffbox-ea1/pyinstall_sdxl_gen/" $MOUNT_POINT \
         --vfs-cache-mode "full" \
         --vfs-write-back "9999h"  \
         --file-perms "0755" \
@@ -13,7 +13,7 @@ yes |  rclone mount  "s3://ffbox-ea1/pyinstall_sdxl_gen/" $MOUNT_POINT \
         --vfs-cache-max-age "24h" \
         --config ~/ffbox/ffbox/rclone.conf \
         --vfs-cache-max-size "100G" \
-        --vfs-read-ahead "10G"  \
+        --vfs-read-ahead "4M"  \
         > "$HOME/rclone_mount.log" 2>&1 &
 # --buffer-size "0" 
 sleep 3  # Wait for 3 seconds to ensure the mount is available
@@ -23,7 +23,7 @@ echo "Start time: $(date)"
 
 cd $MOUNT_POINT
 # ./main/main
-strace -tt -T -e trace=file -o "$HOME/pyinstall_rclone_bench11.log" main/main
+strace -tt -T -e trace=read,file -o "$HOME/strace_rclone.log" main/main
 # strace -tt -T -f -e trace=all -e signal=all -o "$HOME/pyinstall_rclone_bench11.log" main/main
 
 end_time=$(date +%s)
